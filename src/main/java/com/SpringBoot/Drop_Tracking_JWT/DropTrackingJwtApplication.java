7b7348ab44573fbf557a5692a6967dfad135ae2b
Igor-Leonor-Macedo <igl_macedo@hotmail.com>
2025-10-07 19:17:30 -0300

    V.1.0 - Código Inicial.

---
+++ src/main/java/com/SpringBoot/Drop_Tracking_JWT/DropTrackingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Drop_Tracking_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropTrackingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropTrackingJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/Drop_Tracking_JWT/DropTrackingJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Drop_Tracking_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropTrackingJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
